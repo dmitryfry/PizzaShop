fd9c3581ba1f56f8b187a7d5cdcaae631f41d649
Dmitry Fry <im@dmitryfry.com>
2016-06-02 14:23:28 +0300

    Add gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531171110) do
+ActiveRecord::Schema.define(version: 20160531181548) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
